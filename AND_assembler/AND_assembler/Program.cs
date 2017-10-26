aa9b068ff20a6f6d8b055780dddc00ba5ce282f5
Aaron Benson <Aaron Benson@AARONSDESKTOP>
2017-10-25 21:51:44 -0600

    Added Loop to enter a file that exisits. As well as tested logic and started arithmitic

--- AND_assembler/AND_assembler/Program.cs
+++ AND_assembler/AND_assembler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,13 +13,40 @@ namespace AND_assembler
         static void Main(string[] args)
         {
             int pc;
-            String fileName, line;
+            String fileName, line, outFileName;
             Dictionary<string, int> str_pc;
+
+            outFileName = "Error";
 
             str_pc = new Dictionary<string, int>();
 
-            Console.WriteLine("Enter a file. name to assemble");
+            Console.WriteLine("Enter a file name you want to assemble");
             fileName = Console.ReadLine();
+
+
+
+            while(!File.Exists(@"C:\Users\Aaron Benson\Source\Repos\ECE-3710\AND_assembler\AND_Assembler\" + fileName))
+            {
+                Console.WriteLine("File does not exist. Please enter a valid file name.");
+
+                fileName = Console.ReadLine();
+
+            }
+
+
+            // get the name of the file
+            if (!String.IsNullOrWhiteSpace(fileName))
+            {
+                int charLocation = fileName.IndexOf(".", StringComparison.Ordinal);
+
+                if (charLocation > 0)
+                {
+                  outFileName = fileName.Substring(0, charLocation);
+                }
+            }
+
+            // remove any previous instances of the coe file
+            File.Delete(@"C:\Users\Aaron Benson\Source\Repos\ECE-3710\AND_assembler\AND_Assembler\" + outFileName + ".coe");
 
             for (int i = 0; i < 2; i++)
             {
@@ -26,21 +54,30 @@ namespace AND_assembler
                 pc = 0x2800;
 
                 // Read the file and display it line by line.  
-                System.IO.StreamReader file =
-                    new System.IO.StreamReader(@"c:\test.txt");
-                while ((line = file.ReadLine()) != null)
+                System.IO.StreamReader infile =
+                    new System.IO.StreamReader(@"C:\Users\Aaron Benson\Source\Repos\ECE-3710\AND_assembler\AND_Assembler\" + fileName);
+                while ((line = infile.ReadLine()) != null)
                 {
+
                     if (i == 0)
                     {
+                        // Console.WriteLine(line + " and " + "..." + line[0] + "...");
+                        // Console.ReadLine();
                         if (!Char.IsWhiteSpace(line[0]))
                         {
-                            str_pc.Add(line, pc);
+                            string l = line.Substring(0, line.LastIndexOf(':'));
+
+                           // Console.WriteLine(l);
+                           // Console.ReadLine();
+                            str_pc.Add(l, pc);
                             pc++;
                         }
                         else if (isTwoWords(line))
                             pc += 2; //adjust pc for two word instruction
                         else
                             pc++;
+
+
                     }
                     else if (i == 1)
                     {
@@ -52,110 +89,113 @@ namespace AND_assembler
                             // not correct
                             instruction = line.Split(null);
 
-                            switch (instruction[0])
+
+                            //Console.WriteLine(" here " + instruction[1] + " " + instruction[2] + " " + instruction[3] + " \n");
+                            //Console.ReadLine();
+                            switch (instruction[1])
                             {
                                 // logic
                                 case "and":
-                                    toWrite = "000000" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000000" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break; //0000
                                 case "or":
-                                    toWrite = "000001" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000001" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break;
                                 case "not":
-                                    toWrite = "000010" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000010" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break;
                                 case "xor":
-                                    toWrite = "000011" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000011" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break;
 
                                 // arithmetic 
                                 case "addui":
-                                    toWrite = "000101" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
+                                    toWrite = "000101" + immediate_helper(instruction[2], 21) + register_helper(instruction[3]);
                                     break;
                                 case "addsi":
-                                    toWrite = "001001" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
+                                    toWrite = "001001" + immediate_helper(instruction[2], 21) + register_helper(instruction[3]);
                                     break;
                                 case "subui":
-                                    toWrite = "000111" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
+                                    toWrite = "000111" + immediate_helper(instruction[2], 21) + register_helper(instruction[3]);
                                     break;
                                 case "subsi":
-                                    toWrite = "001011" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
+                                    toWrite = "001011" + immediate_helper(instruction[2], 21) + register_helper(instruction[3]);
                                     break;
 
                                 case "addu":
-                                    toWrite = "000100" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000100" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break; // 0001
 
                                 case "subu":
-                                    toWrite = "000110" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000110" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break; // 0001
 
                                 case "adds":
-                                    toWrite = "001000" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001000" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break;// 0010
 
                                 case "subs":
-                                    toWrite = "001010" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001010" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break; // 0010
 
                                 case "shiftra":
-                                    toWrite = "001100" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001100" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break; // 0011
 
                                 case "shiftrl":
-                                    toWrite = "001110" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001110" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break;// 0011
 
                                 case "shiftrai":
-                                    toWrite = "001101" + immediate_helper(instruction[1], 5) + register_helper(instruction[2]);
+                                    toWrite = "001101" + immediate_helper(instruction[2], 5) + register_helper(instruction[3]);
                                     break; // 0011
 
                                 case "shiftrli":
-                                    toWrite = "001111" + immediate_helper(instruction[1], 5) + register_helper(instruction[2]);
+                                    toWrite = "001111" + immediate_helper(instruction[2], 5) + register_helper(instruction[3]);
                                     break;// 0011
 
                                 case "shiftl":
-                                    toWrite = "010000" + register_helper(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "010000" + register_helper(instruction[2]) + register_helper(instruction[3]);
                                     break; // 0100
 
                                 case "jmpl":
-                                    toWrite = "010100" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "010100" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0101
 
                                 case "jmple":
-                                    toWrite = "010101" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "010101" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0101
 
                                 case "jmpb":
-                                    toWrite = "010110" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "010110" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0101
 
                                 case "jmpbe":
-                                    toWrite = "010111" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "010111" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0101
 
                                 case "jmpg":
-                                    toWrite = "011000" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "011000" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0110
 
                                 case "jmpge":
-                                    toWrite = "011001" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "011001" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0110
 
                                 case "jmpa":
-                                    toWrite = "011010" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "011010" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; //0110
 
                                 case "jmpae":
-                                    toWrite = "011011" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "011011" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; //0110
 
                                 case "jmpe":
-                                    toWrite = "011100" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "011100" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0111
 
                                 case "jmpne":
-                                    toWrite = "011110" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "011110" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 0111
 
                                     // needs to handle 
@@ -168,11 +208,11 @@ namespace AND_assembler
 
                                     // does this just store the data into a specified area of memory or a specific register?
                                 case "jmpsra":
-                                    toWrite = "100010" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "100010" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; // 1000
 
                                 case "jmptra":
-                                    toWrite = "100011" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
+                                    toWrite = "100011" + register_helper(instruction[2]) + register_helper(instruction[3]) + immediate_helper(instruction[4], 16);
                                     break; //1000
 
 
@@ -182,7 +222,7 @@ namespace AND_assembler
                                            break; // 1001
                                            */
                                 case "load":
-                                    toWrite = load_write_helper(instruction[1]) + immediate_helper(instruction[2], 24);
+                                    toWrite = load_write_helper(instruction[2]) + immediate_helper(instruction[3], 24);
                                     break;
                                 /*
                             case "loadh":
@@ -198,17 +238,20 @@ namespace AND_assembler
 
 
                                 case "write":
-                                    toWrite = load_write_helper(instruction[1]) + immediate_helper(instruction[2], 24);
+                                    toWrite = load_write_helper(instruction[2]) + immediate_helper(instruction[3], 24);
                                     break;
 
                                 case "push":
-                                    toWrite = "1101" + "0" + register_helper(instruction[1]) + immediate_helper(instruction[2], 6); // instruction[2] = to something like 0x1f
+                                    toWrite = "1101" + "0" + register_helper(instruction[2]) + immediate_helper(instruction[3], 6); // instruction[2] = to something like 0x1f
                                     break; // 1101 0
 
                                 case "pop":
-                                    toWrite = "1101" + "1" + register_helper(instruction[1]) + immediate_helper(instruction[2], 6); // instruction[2] = to something like 010110
+                                    toWrite = "1101" + "1" + register_helper(instruction[2]) + immediate_helper(instruction[3], 6); // instruction[2] = to something like 010110
                                     break; // 1101 1
-                                           //        
+                                           //       
+                                default:
+                                    toWrite = "didn't work";
+                                    break;
                             }
 
 
@@ -217,10 +260,27 @@ namespace AND_assembler
                             else
                                 pc++;
 
+
+
+                            using (System.IO.StreamWriter outfile = new System.IO.StreamWriter(@"C:\Users\Aaron Benson\Source\Repos\ECE-3710\AND_assembler\AND_Assembler\" + outFileName + ".coe", true))
+                            {
+                                if (toWrite.Length == 16)
+                                    outfile.WriteLine(toWrite);
+                                else if (toWrite.Length == 32)
+                                {
+                                    string first = toWrite.Substring(0, 16);
+                                    string second = toWrite.Substring(16).Trim();
+                                    outfile.WriteLine(first);
+                                    outfile.WriteLine(second);
+                                }
+                                else
+                                    outfile.WriteLine("Compile Error" + toWrite + "\n");
+
+                            }
                         }
                     }
                 }
-                file.Close();
+                infile.Close();
             }
         }
 
@@ -273,10 +333,10 @@ namespace AND_assembler
             string result = reg.Substring(reg.LastIndexOf('r') + 1);
 
             Int32.TryParse(result, out i);
-
+            
             string toRet = Convert.ToString(i, 2).PadLeft(5, '0');
 
-            return null;
+            return toRet;
         }
 
         // convert a string immediate to a binary string (e.g 0x1f -> 11111)
@@ -285,10 +345,11 @@ namespace AND_assembler
             int i;
             string result = immd.Substring(immd.LastIndexOf('x') + 1);
             
-            Int32.TryParse(result, out i);
-
+            // Int32.TryParse(result, out i);
+            i = Convert.ToInt32(result, 16);
+            
             string s = Convert.ToString(i, 2).PadLeft(bits, '0');
-
+           
             return s;
         }
         
