ca07834329a7e40fa1036454ee8bbe5bd9180390
Aaron Benson <benson.aaron.k@gmail.com>
2017-10-25 19:44:01 -0600

    fixed immediate helper

--- AND_assembler/AND_assembler/Program.cs
+++ AND_assembler/AND_assembler/Program.cs
@@ -70,16 +70,16 @@ namespace AND_assembler
 
                                 // arithmetic 
                                 case "addui":
-                                    toWrite = "000101" + immediate_helper_16(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000101" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
                                     break;
                                 case "addsi":
-                                    toWrite = "001001" + immediate_helper_16(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001001" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
                                     break;
                                 case "subui":
-                                    toWrite = "000111" + immediate_helper_16(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "000111" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
                                     break;
                                 case "subsi":
-                                    toWrite = "001011" + immediate_helper_16(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001011" + immediate_helper(instruction[1], 16) + register_helper(instruction[2]);
                                     break;
 
                                 case "addu":
@@ -107,11 +107,11 @@ namespace AND_assembler
                                     break;// 0011
 
                                 case "shiftrai":
-                                    toWrite = "001101" + immediate_helper_5(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001101" + immediate_helper(instruction[1], 5) + register_helper(instruction[2]);
                                     break; // 0011
 
                                 case "shiftrli":
-                                    toWrite = "001111" + immediate_helper_5(instruction[1]) + register_helper(instruction[2]);
+                                    toWrite = "001111" + immediate_helper(instruction[1], 5) + register_helper(instruction[2]);
                                     break;// 0011
 
                                 case "shiftl":
@@ -119,63 +119,70 @@ namespace AND_assembler
                                     break; // 0100
 
                                 case "jmpl":
-                                    toWrite = "010100" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "010100" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0101
 
                                 case "jmple":
-                                    toWrite = "010101" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "010101" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0101
 
                                 case "jmpb":
-                                    toWrite = "010110" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "010110" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0101
 
                                 case "jmpbe":
-                                    toWrite = "010111" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "010111" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0101
 
                                 case "jmpg":
-                                    toWrite = "011000" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "011000" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0110
 
                                 case "jmpge":
-                                    toWrite = "011001" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "011001" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0110
 
                                 case "jmpa":
-                                    toWrite = "011010" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "011010" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; //0110
 
                                 case "jmpae":
-                                    toWrite = "011011" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "011011" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; //0110
 
                                 case "jmpe":
-                                    toWrite = "011100" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "011100" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0111
 
                                 case "jmpne":
-                                    toWrite = "011110" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "011110" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 0111
 
+                                    // needs to handle 
                                 case "jmp":
-                                    toWrite = "100000" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+
+                                    string pc_for_label = str_pc[instruction[2]].ToString();
+
+                                    toWrite = "100000" + "00" + pc_for_label; // register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
                                     break; // 1000
 
+                                    // does this just store the data into a specified area of memory or a specific register?
                                 case "jmpsra":
-                                    toWrite = "100010" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "100010" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; // 1000
 
                                 case "jmptra":
-                                    toWrite = "100011" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
+                                    toWrite = "100011" + register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper(instruction[3], 16);
                                     break; //1000
+
+
                                            /*
                                        case "loadl":
                                           // toWrite = "100010" + "0" register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
                                            break; // 1001
                                            */
                                 case "load":
-                                    toWrite = load_helper(instruction[1]) + immediate_helper_24(instruction[2]);
+                                    toWrite = load_write_helper(instruction[1]) + immediate_helper(instruction[2], 24);
                                     break;
                                 /*
                             case "loadh":
@@ -191,14 +198,17 @@ namespace AND_assembler
 
 
                                 case "write":
+                                    toWrite = load_write_helper(instruction[1]) + immediate_helper(instruction[2], 24);
                                     break;
 
                                 case "push":
-                                    break; // 1101
+                                    toWrite = "1101" + "0" + register_helper(instruction[1]) + immediate_helper(instruction[2], 6); // instruction[2] = to something like 0x1f
+                                    break; // 1101 0
 
                                 case "pop":
-                                    break; // 1101
-
+                                    toWrite = "1101" + "1" + register_helper(instruction[1]) + immediate_helper(instruction[2], 6); // instruction[2] = to something like 010110
+                                    break; // 1101 1
+                                           //        
                             }
 
 
@@ -239,10 +249,12 @@ namespace AND_assembler
                 case "jmp":
                 case "jmpsra":
                 case "jmptra":
-                case "loadl":
-                case "loadh":
-                case "writel":
-                case "writeh":
+                case "load":
+                case "write":
+                //case "loadl":
+                //case "loadh":
+                //case "writel":
+                //case "writeh":
                 case "push":
                 case "pop":
                     return true;
@@ -255,46 +267,56 @@ namespace AND_assembler
         /*
          * return the 5 binary string of the register
          */
-        static private string register_helper(string reg1)
+        static private string register_helper(string reg)
         {
+            int i;
+            string result = reg.Substring(reg.LastIndexOf('r') + 1);
+
+            Int32.TryParse(result, out i);
+
+            string toRet = Convert.ToString(i, 2).PadLeft(5, '0');
 
             return null;
         }
 
-        // change long to 16
-        static private string immediate_helper_16(string immmd)
+        // convert a string immediate to a binary string (e.g 0x1f -> 11111)
+        static private string immediate_helper(string immd, int bits)
         {
-            return null;
-        }
+            int i;
+            string result = immd.Substring(immd.LastIndexOf('x') + 1);
+            
+            Int32.TryParse(result, out i);
 
-        // change shortr to 5
-        static private string immediate_helper_5(string immmd)
-        {
-            return null;
-        }
-        static private string immediate_helper_24(string immmd)
-        {
-            return null;
-        }
+            string s = Convert.ToString(i, 2).PadLeft(bits, '0');
 
-        static private string load_helper(string register)
+            return s;
+        }
+        
+        static private string load_write_helper(string register)
         {
             int j;
             j = register_num(register);
 
-            if (j <= 16)
+            if (j < 16) // I think this has to be < 16
                 return "1001" + Convert.ToString(j, 2).PadLeft(4, '0');
             else
                 return "1010" + Convert.ToString(j & 0xF, 2).PadLeft(4, '0');
 
         }
-
+        
         static private int register_num(string reg)
         {
             int i;
             string temp = reg.Substring(1);
             Int32.TryParse(temp, out i);
             return i;
+        }
+
+        public static string toLittleEndian(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
         }
     }
 }
