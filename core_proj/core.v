ab5192bcc65d575cbc45533e586f5dac4379399e
nick elliott <Nick.jwl123@gmail.com>
2017-10-27 15:12:20 -0600

    REV 1: Implemented core.v, core_top.v, memory_manager.v, generated block RAM

--- core_proj/core.v
+++ core_proj/core.v
@@ -18,19 +18,53 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module core(	input clk,
-
-				input[15:0] mem_out,
+module core(	
+				input clk,
+				input[23:0] mem_out,
 				input[7:0] servo_in,
 				input[7:0] ir_in,
 				output[23:0] mem_addr,
 				output[7:0] ir_out,
 				output[7:0] servo_out,
 
-
-
-
     		);
+
+	reg[2:0] state = 4'd0;
+
+	////// STATES ///////
+	param fetch = 0;
+	param fetch = 1;
+	param fetch = 2;
+	param fetch = 3;
+	param fetch = 4;
+	param fetch = 5;
+	param fetch = 6;
+	param fetch = 7;
+
+	//////////////  STATES  ////////////////////
+
+		//////////////  SEQUANTIAL STATES  ////////////////////
+
+		always @(posedge clk)
+		begin
+			case(state)
+			3'd0:	
+			3'd1:
+			3'd2:
+			3'd3:
+			3'd4:
+			3'd5:
+			3'd6:
+			3'd7:
+
+		end
+
+		//////////////  COMBINATORIAL STATES  ////////////////////
+
+		always @(*)
+		begin
+
+		end
 
 
 endmodule
--- core_proj/core_top.v
+++ core_proj/core_top.v
@@ -25,7 +25,36 @@ module core_top(
 			    );
 
 
-				// Instantiate BLOCK MEM
+wire[] mem_out_to_core	
+
+
+				core core1(
+							.clock(clk)
+				 			.mem_out(),				//input[23:0]
+				 			.servo_in(),				//input[7:0] 
+				 			.ir_in(),				//input[7:0] 
+				 			.mem_addr(),				//output[23:0]
+				 			.ir_out(),				//output[7:0]
+				 			.servo_out(),				//output[7:0]
+    						);
+
+				
+
+				// Instantiate mem_manager
+				mem_manager mem_manager1(
+											.clock(clk)			//input				clock,
+											.//input[23:0] 		addr_in_block1,						
+														//input[23:0] 		addr_in_block2,
+														//input[15:0]			data_in1,
+														//input[15:0]			data_in2,
+														//input				req,
+														//input				read_write1,
+														//input				read_write2,
+														//output reg[15:0]	data_out1,
+														//output reg[15:0]	data_out2,
+
+
+											);
 
 				// INSTANTIATE CELL MEM
 
