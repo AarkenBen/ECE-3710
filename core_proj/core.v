d7af8fea8ed6852fd93e1c6350de048ef46aa28c
nick elliott <Nick.jwl123@gmail.com>
2017-10-27 12:28:46 -0600

    Second Attempt at .v files

---
+++ core_proj/core.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:26:59 10/26/2017 
+// Design Name: 
+// Module Name:    core 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module core(	input clk,
+
+				input[15:0] mem_out,
+				input[7:0] servo_in,
+				input[7:0] ir_in,
+				output[23:0] mem_addr,
+				output[7:0] ir_out,
+				output[7:0] servo_out,
+
+
+
+
+    		);
+
+
+endmodule
---
+++ core_proj/core_top.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:20:25 10/26/2017 
+// Design Name: 
+// Module Name:    core_top 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module core_top(		
+					input			 	clk,
+					output[7:0] 		VGA_pad,
+					output[7:0]			servo_pad,
+			    );
+
+
+				// Instantiate BLOCK MEM
+
+				// INSTANTIATE CELL MEM
+
+				// INSTANTIATE SERVO CONTROL
+
+				// INSTANTIATE IR SENSOR DECODER
+
+				// INSTANTIATE CORE
+
+endmodule
