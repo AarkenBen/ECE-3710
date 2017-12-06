a8432bb1fc5955e59ecdcf55aa1bb5c40d04b7c8
nick elliott <Nick.jwl123@gmail.com>
2017-12-06 16:47:05 -0700

    fun stuff

--- IR_repositioning/ipcore_dir/block_mem_xmdf.tcl
+++ IR_repositioning/ipcore_dir/block_mem_xmdf.tcl
@@ -223,10 +223,6 @@ utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path block_mem.ng
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type ngc
 incr fcount
 
-utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path block_mem.sym
-utilities_xmdf::xmdfSetData $instance FileSet $fcount type symbol
-incr fcount
-
 utilities_xmdf::xmdfSetData $instance FileSet $fcount relative_path block_mem.v
 utilities_xmdf::xmdfSetData $instance FileSet $fcount type verilog
 incr fcount
