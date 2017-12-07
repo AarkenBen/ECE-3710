a4d3555cbbc0ac45124fa3411e09e2214ed459a2
radboy16 <imchillin2day@gmail.com>
2017-12-06 18:31:27 -0700

    maybe this is the one

--- AND_assembler/AND_assembler/Program.cs
+++ AND_assembler/AND_assembler/Program.cs
@@ -207,7 +207,7 @@ namespace AND_assembler
                                     break; // 1000
 
                                 case "jmpb":
-                                    pc_for_label = immediate_helper(str_pc[instruction[4]].ToString("X"), 24);
+                                    pc_for_label = immediate_helper(str_pc[instruction[2]].ToString("X"), 24);
 
                                     toWrite = "010110" + "00" + pc_for_label; // register_helper(instruction[1]) + register_helper(instruction[2]) + immediate_helper_16(instruction[3]);
                                     break; // 1000
